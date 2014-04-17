209ce5f7349bb187a924fe28a4adce0fb720b648
Pepijn de Vos <pepijndevos@gmail.com>
2014-04-17 17:29:02 +0200

    more accurate buffer count

--- pypredis/sendbuffer.py
+++ pypredis/sendbuffer.py
@@ -5,22 +5,25 @@ from cStringIO import StringIO
 class SendBuffer(object):
 
     def __init__(self, max_size):
+        self.count = 0
         self.mark = 0
         self.current = ''
         self.buf = Queue(max_size)
 
     def __len__(self):
-        return self.buf.qsize() + int(bool(self.current))
+        return self.buf.qsize() + self.count
 
     def write(self, data):
         self.buf.put(data)
 
     def to_sock(self, sock):
         if self.mark >= len(self.current):
+            self.count = 0
             io = StringIO()
             while True:
                 try:
                     io.write(self.buf.get_nowait())
+                    self.count += 1
                 except Empty:
                     break
 
